f73b2400295c7eace1d41f297ee51c42b219c363
--replace-all <alexocean.e.k@gmail.com>
2024-06-12 12:31:57 +0500

    scrollbar-gutter

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ const burger = document.querySelector('.burger-button');
 const backdrop = document.querySelector('.backdrop');
 const handleSideClick = () => {
     burger.setAttribute('aria-expanded', 'false');
+    document.body.classList.remove('no-overflow');
 }
 burger.addEventListener('click', () => {
     const expanded = burger.getAttribute('aria-expanded') === 'true' || false;
     burger.setAttribute('aria-expanded', !expanded);
     if (!expanded) {
         backdrop.addEventListener('click', handleSideClick);
+        document.body.classList.add('no-overflow');
     } else {
         backdrop.removeEventListener('click', handleSideClick);
+        document.body.classList.remove('no-overflow');
     }
 })
